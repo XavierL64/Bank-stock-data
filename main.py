025fd986a807c8a22d5a31650fd133ac8846175e
XavierL64 <118341547+XavierL64@users.noreply.github.com>
2023-03-22 07:42:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,29 @@ bank_stocks = pd.concat([BAC, C, GS, JPM, MS, WFC],axis=1,keys=tickers)
 
 # set column name levels
 bank_stocks.columns.names = ['Bank Ticker','Stock Info']
+
+# find max close price for each bank's stock
+bank_stocks.xs(key='Close',axis=1,level='Stock Info').max()
+
+# create a dataframe with the returns for each bank's stock
+returns = pd.DataFrame()
+
+for tick in tickers:
+    returns[tick + ' Return'] = bank_stocks[tick]['Close'].pct_change()
+ 
+# create a pairplot of the returns dataframe using seaborn
+sns.pairplot(returns)
+
+# find dates each bank stock had the best and worst single day returns
+returns.idxmax()
+returns.idxmin()
+
+# find standard deviation of the returns over the entire period and for specific year
+returns.std()
+returns.loc['2015-01-01':'2015-12-31'].std()
+
+# create a histogram of returns achieved in specfic year and stock using seaborn
+sns.histplot(returns.loc['2015-01-01':'2015-12-31']['MS Return'],color='green',bins=100)
+sns.histplot(returns.loc['2008-01-01':'2008-12-31']['C Return'],color='red',bins=100)
+
+
